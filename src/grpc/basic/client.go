cfb0f65e444b0ca9be3e94d211cf75094d5de2a6
JDNew <chijiaduo@hotmail.com>
2022-12-25 15:10:00 +0800

    feat: grpc token auth

--- src/grpc/basic/client.go
+++ src/grpc/basic/client.go
@@ -8,10 +8,29 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+type ClientTokenAuth struct {
+}
+
+// GetRequestMetadata 实现 grpc 的 credentials 包下的两个 token 认证接口
+func (auth *ClientTokenAuth) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"appId":  "hello",
+		"appKey": "123123",
+	}, nil
+}
+
+func (auth *ClientTokenAuth) RequireTransportSecurity() bool {
+	return false
+}
+
 func main() {
 
+	var opts []grpc.DialOption
 	// 选择禁用安全传输，无加密和验证
-	conn, err := grpc.Dial("127.0.0.1:8028", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(opts, grpc.WithPerRPCCredentials(new(ClientTokenAuth)))
+
+	conn, err := grpc.Dial("127.0.0.1:8028", opts...)
 	if err != nil {
 		panic(err)
 	}
--- src/grpc/basic/server.go
+++ src/grpc/basic/server.go
@@ -3,7 +3,10 @@ package main
 import (
 	helloPb "Go_Code/src/grpc/protopb/hello"
 	"context"
+	"errors"
+	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"net"
 )
 
@@ -30,6 +33,29 @@ func main() {
 }
 
 func (s *Server) SayHello(ctx context.Context, req *helloPb.HelloReq) (*helloPb.HelloResp, error) {
+	// 获取元数据的信息，实际是这部分 token 的东西写在拦截器里
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errors.New("no token")
+	}
+
+	fmt.Printf("md = %v", md)
+	var (
+		appId, appKey string
+	)
+
+	if v, ok := md["appid"]; ok {
+		appId = v[0]
+	}
+
+	if v, ok := md["appkey"]; ok {
+		appKey = v[0]
+	}
+	fmt.Printf("appId = %v, appKey = %v\n", appId, appKey)
+	if appId != "hello" || appKey != "123123" {
+		return nil, errors.New("token wrong")
+	}
+
 	return &helloPb.HelloResp{
 		Msg: "hello " + req.GetName()}, nil
 }
